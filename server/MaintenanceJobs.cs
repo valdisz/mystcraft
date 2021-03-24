e69d43fc5416c8f4111348d7a5178813ae8cbaa2
Valdis Zobēla <valdis@zobela.eu>
2021-03-24 19:14:15 +0200

    maintenance job placeholder

---
+++ server/MaintenanceJobs.cs
@@ -0,0 +1,36 @@
+namespace advisor
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using advisor.Persistence;
+    using Hangfire.RecurringJobExtensions;
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging;
+
+    public class MaintenanceJobs {
+        public MaintenanceJobs(Database db, IMediator mediator, ILogger<RemoteGameServerJobs> logger) {
+            this.db = db;
+            this.mediator = mediator;
+            this.logger = logger;
+        }
+
+        private readonly Database db;
+        private readonly IMediator mediator;
+        private readonly ILogger logger;
+
+
+        [RecurringJob("0 0 * * *", TimeZone = "America/Los_Angeles")]
+        public async Task ReconcileRports() {
+            logger.LogInformation($"Looking for unparsed reports");
+
+            var reportsWithoutJson = await db.Reports
+                .Include(x => x.Turn)
+                .ThenInclude(x => x.Player)
+                .Where(x => x.Json == null)
+                .ToListAsync();
+
+            logger.LogInformation($"Found {reportsWithoutJson.Count} unparsed reports");
+        }
+    }
+}
