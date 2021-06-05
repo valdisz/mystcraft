bddb91876c132b2fa18568775e1f1c8e7de09944
Valdis Zobēla <valdis@zobela.eu>
2021-06-05 13:36:02 +0300

    get player report by GM

--- server/ReportsController.cs
+++ server/ReportsController.cs
@@ -11,6 +11,8 @@ namespace advisor {
     using HotChocolate.Types.Relay;
     using advisor.Features;
     using advisor.Persistence;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
 
     [Authorize]
     [Route("api")]
@@ -50,6 +52,27 @@ namespace advisor {
             await mediator.Send(new ProcessTurn(playerIdValue, earliestTurn));
 
             return Ok();
+        }
+
+        [HttpGet("{playerId}/report/{turnNumber}/{factionNumber}")]
+        [Authorize(Roles = Roles.GameMaster)]
+        public async Task<IActionResult> UploadRuleset([Required, FromRoute] string playerId, [Required, FromRoute] int turnNumber, [Required, FromRoute] int factionNumber) {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var relayPlayerId = relayId.Deserialize(playerId);
+            if (relayPlayerId.TypeName != "Player") return BadRequest();
+
+            var playerIdValue = (long) relayPlayerId.Value;
+
+            var report = await database.Reports
+                .Include(x => x.Turn)
+                .Where(x => x.PlayerId == playerIdValue && x.Turn.Number == turnNumber && x.FactionNumber == factionNumber)
+                .Select(x => x.Source)
+                .SingleOrDefaultAsync();
+
+            if (report == null) return NotFound();
+
+            return Content(report, "text");
         }
 
         [HttpPost("{playerId}/map")]
