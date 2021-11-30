a514f54f0412f54617777d52648c6f2017ce62d7
Valdis Zobēla <valdis@zobela.eu>
2021-11-30 13:06:01 +0200

    renamed private memgers in the PriorityQueue

--- client/src/game/orders/orders.ts
+++ client/src/game/orders/orders.ts
@@ -1,26 +1,10 @@
-import { Direction } from "../../schema";
-import { Link } from "../link";
-import { Unit } from "../unit";
+import { Direction } from '../../schema'
+import { Region } from '../region'
+import { Ruleset } from '../ruleset'
+import { Structure } from '../structure'
+import { Unit } from '../unit'
 
-export abstract class BaseOrder {
-    protected ensure(predicate: () => boolean, errorMessage: string) {
-        if (!this.isValid) return
-
-        if (!predicate()) {
-            this.isValid = false
-            this.error = errorMessage
-        }
-    }
-
-    isValid: boolean
-    error: string
-
-    abstract getOrder(): number
-    abstract toString(): string
-    abstract execute(): Partial<Unit>
-}
-
-function toDirection(dir: Direction) {
+function directionToString(dir: Direction) {
     switch (dir) {
         case Direction.North: return 'n'
         case Direction.Northeast: return 'ne'
@@ -32,26 +16,100 @@ function toDirection(dir: Direction) {
     }
 }
 
+export type MoveStep = Direction | 'in' | 'out' | number
+
+export abstract class BaseOrder {
+    protected ensure(predicate: () => boolean, errorMessage: string) {
+        if (!this.isValid) return false
+
+        if (!predicate()) {
+            this.setError(errorMessage)
+            return false
+        }
+
+        this.clearError()
+        return true
+    }
+
+    isValid: boolean
+    error: string
+
+    setError(message: string) {
+        this.isValid = false
+        this.error = message
+    }
+
+    clearError() {
+        this.isValid = true
+        this.error = null
+    }
+
+    abstract toString(): string
+    abstract execute(): Partial<Unit>
+}
+
 export class Move extends BaseOrder {
-    constructor (public unit: Unit, public path: Link[]) {
+    constructor (private ruleset: Ruleset, public unit: Unit, public path: MoveStep[]) {
         super()
 
         this.ensure(() => !!path.length, 'Move path is not set')
     }
 
-    getOrder(): number {
-        return 1
-    }
-
     execute(): Partial<Unit> {
-        return {
-            path: this.path
+        let curStructure: Structure = this.unit.structure
+        let curRegion: Region = this.unit.region
+
+        let usedMp = 0
+        const S = [...this.path].reverse()
+        const P = []
+
+        loop:
+        while (S.length > 0) {
+            const step = S.pop()
+
+            if (typeof step === 'number') {
+                const nextStructure = curRegion.structures.find(s => s.num == step)
+                if (!this.ensure(() => !!nextStructure, `Cannot find structure with number ${step} in the region ${curRegion}`)) {
+                    break
+                }
+
+                P.push(step)
+                continue
+            }
+
+            switch (step) {
+                case 'in':
+                    // todo: out of scope, IN moves through the shaft/static portal in nexus.
+                    P.push(step)
+                    break loop
+
+                case 'out':
+                    // if we are in the region then OUT will do nothing
+                    if (curStructure) {
+                        // otherwise we will end up in the region
+                        curStructure = null
+                    }
+
+                    P.push(step)
+                    break
+
+                default:
+                    const link = curRegion.neighbors.find(x => x.direction == step)
+                    if (!this.ensure(() => !!link, `There is no exit in ${step} direction in the region ${curRegion}`)) {
+                        break loop
+                    }
+
+                    const cost = curRegion.terrain.getMoveCost(this.unit.capacity)
+            }
         }
+
+        return {}
     }
 
     toString(): string {
+
         return this.isValid
-            ? `move ${this.path.map(x => toDirection(x.direction)).join(' ')}`
+            ? `move ${this.path.map(x => d.to).join(' ')}`
             : `move; ${this.error}`
     }
 }
