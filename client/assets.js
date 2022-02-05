38755eb3ef5e7daedfe4a6b745db58a9d37b1ea9
Valdis Zobēla <valdis@zobela.eu>
2022-02-05 15:31:09 +0200

    assets

--- client/assets.js
+++ client/assets.js
@@ -1,20 +1,70 @@
 // required ImageMagic installed globaly
+const os = require('os')
+os.tmpDir = os.tmpdir
+
+const fs = require('fs')
 
 const spritesheet = require('spritesheet-js')
 
 const options = {
     format: 'pixi.js',
-    path: 'assets'
+    path: 'static',
 }
 
-spritesheet('./src/assets/terrain/advisor/*.png', { ...options, name: 'advisor-terrain' }, function (err) {
+const optionsCss = {
+    format: 'css',
+    path: 'static'
+}
+
+spritesheet('./src/assets/terrain/*.png', { ...options, name: 'terrain' }, function (err) {
     if (err) throw err;
 
-    console.log('Advisor terrain spritesheet... DONE');
+    console.log('Terrain spritesheet... DONE');
 })
 
-spritesheet('./src/assets/terrain/fantasy/*.png', { ...options, name: 'fantasy-terrain' }, function (err) {
+spritesheet('./src/assets/items/*.png', { ...options, name: 'items' }, function (err) {
     if (err) throw err;
 
-    console.log('Fantasy terrain spritesheet... DONE');
+    console.log('Items spritesheet... DONE');
+})
+
+spritesheet('./src/assets/objects/*.png', { ...options, name: 'objects' }, function (err) {
+    if (err) throw err;
+
+    console.log('Objects spritesheet... DONE');
+})
+
+spritesheet('./src/assets/items/*.png', { ...optionsCss, prefix: 'item-', name: 'items-css' }, function (err) {
+    if (err) throw err;
+
+    console.log('Items CSS spritesheet... DONE');
+})
+
+spritesheet('./src/assets/objects/*.png', { ...optionsCss, prefix: 'object-', name: 'objects-css' }, function (err) {
+    if (err) throw err;
+
+    const fname = 'static/objects-css.css'
+
+    const f = fs.readFileSync(fname, 'UTF-8')
+    const lines = f.split(/\n/)
+
+    const outf = fs.openSync(fname, 'w+')
+    for (const l of lines) {
+        if (l.startsWith('.')) {
+            const rule = l.match(/^(.+) {$/i)[1]
+            if (rule) {
+                const fixedRule = rule.replace(/(\s|')+/gi, '-').toLowerCase()
+                console.log(`${rule} -> ${fixedRule}`)
+
+                fs.writeFileSync(outf, `${fixedRule} {\n`)
+                continue
+            }
+        }
+
+        fs.writeFileSync(outf, `${l}\n`)
+    }
+    fs.closeSync(outf)
+
+
+    console.log('Objects CSS spritesheet... DONE');
 })
