153ced52e19d78a17be81813f796eb8f31817171
Valdis Zobēla <valdis@zobela.eu>
2022-07-12 16:27:30 +0300

    copyright w/o link in it

--- client/src/components/copyright.tsx
+++ client/src/components/copyright.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 
 export function Copyright() {
     return (
         <Typography variant='body2' color='textSecondary' align='center'>
-            { `Copyright ©  ${new Date().getFullYear()} ` }
-            <Link color='inherit' href='https://advisor.azurewebsites.net'>Valdis Zobēla</Link>
+            { `Copyright ©  ${new Date().getFullYear()} Valdis Zobēla` }
         </Typography>
     );
 }
