ab5619336acf999c629d741c29a35e66134a5855
Valdis Zobēla <valdis@zobela.eu>
2022-08-07 21:14:08 +0300

    users page use correct layout

--- client/src/pages/users-page.tsx
+++ client/src/pages/users-page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Container } from '@mui/material'
+import { PageTitle } from '../components'
 
 export function UsersPage() {
-    return <div>Users</div>
+    return <Container>
+        <PageTitle title='Users'/>
+    </Container>
 }
