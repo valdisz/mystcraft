e900cccf78dcc45cbaee4113696e5fa1688f7abc
Valdis Zobēla <valdis@zobela.eu>
2023-02-02 00:04:58 +0200

    changes

--- server/WellKnownClaimTypes.cs
+++ server/WellKnownClaimTypes.cs
@@ -1,9 +1,9 @@
-namespace advisor {
-    public static class WellKnownClaimTypes {
-        public const string UserId = "advisor.id";
-        public const string Email = "advisor.email";
-        public const string Role = "advisor.role";
-        public const string Player = "advisor.player";
-        public const string Timestamp = "advisor.ts";
-    }
+namespace advisor;
+
+ublic static class WellKnownClaimTypes {
+    public const string UserId = "advisor.id";
+    public const string Email = "advisor.email";
+    public const string Role = "advisor.role";
+    public const string Player = "advisor.player";
+    public const string Timestamp = "advisor.ts";
 }
