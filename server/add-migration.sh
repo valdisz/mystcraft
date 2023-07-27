0f15303dab4994fa17d1720fb361fc7627aa3816
Valdis Zobēla <valdis@zobela.eu>
2023-07-27 19:29:48 +0300

    better migration script

--- server/add-migration.sh
+++ server/add-migration.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-dotnet build
-dotnet ef migrations add $1 --no-build --context SQLiteDatabase --output-dir Migrations/sqlite -- Provider=SQLite
-dotnet ef migrations add $1 --no-build --context PgSqlDatabase --output-dir Migrations/pgsql -- Provider=PgSQL
-dotnet ef migrations add $1 --no-build --context MsSqlDatabase --output-dir Migrations/mssql -- Provider=MsSQL
+
+if dotnet build ; then
+    dotnet ef migrations add $1 --no-build --context SQLiteDatabase --output-dir Migrations/sqlite -- Provider=SQLite
+    dotnet ef migrations add $1 --no-build --context PgSqlDatabase --output-dir Migrations/pgsql -- Provider=PgSQL
+    dotnet ef migrations add $1 --no-build --context MsSqlDatabase --output-dir Migrations/mssql -- Provider=MsSQL
+else
+    echo "Build failed. No migrations were created."
+fi
+
